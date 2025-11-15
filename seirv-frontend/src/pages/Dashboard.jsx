16e112dc4ef4d12755dec43cd415185d84e23fc2
Estebin21 <esteban.narvaez.acurio@udla.edu.ec>
2025-11-15 00:57:39 -0500

    Configuración primer usuario admin y dashboard para redirigir login

---
+++ seirv-frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: "2rem", color: "white" }}>
+      <h1>Dashboard</h1>
+      <p>Bienvenido al sistema SEIRV.</p>
+    </div>
+  );
+}
--- seirv-frontend/src/pages/Login.jsx
+++ seirv-frontend/src/pages/Login.jsx
@@ -1,10 +1,38 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import InputField from "../components/InputField";
+import axios from "axios";
 
 export default function Login() {
-  const [usuario, setUsuario] = useState("");
-  const [clave, setClave] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const payload = {
+        username: username,
+        password: password,
+      };
+
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/auth/login",
+        payload
+      );
+
+      const token = response.data.access_token;
+
+      // Guardar token
+      localStorage.setItem("token", token);
+
+      // Redirigir al dashboard
+      navigate("/dashboard");
+
+    } catch (error) {
+      console.error("Error al iniciar sesión:", error);
+      alert("Credenciales incorrectas");
+    }
+  };
 
   return (
     <div className="card-container">
@@ -12,22 +40,22 @@ export default function Login() {
       <p className="subtitle">Bienvenido al Sistema SEIRV</p>
 
       <InputField
-        label="Usuario o Email"
+        label="Usuario"
         type="text"
-        placeholder="ej: jlopez / jlopez@mail.com"
-        value={usuario}
-        onChange={(e) => setUsuario(e.target.value)}
+        placeholder="tu usuario"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
 
       <InputField
         label="Contraseña"
         type="password"
-        placeholder="Tu contraseña"
-        value={clave}
-        onChange={(e) => setClave(e.target.value)}
+        placeholder="tu contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
 
-      <button>Ingresar</button>
+      <button onClick={handleLogin}>Ingresar</button>
 
       <Link to="/register" className="link">¿No tienes cuenta? Crear una cuenta</Link>
     </div>
